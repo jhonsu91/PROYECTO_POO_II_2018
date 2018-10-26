f69bdc62993332578230249f88b82cdbf1efa18d
j_h4e <j_h4e@DESKTOP-A42TOC9.mshome.net>
2018-10-26 11:48:34 -0500

    Se declararon las clases TDA bases para Amazon Viewer

---
+++ src/com/jsuntaxi/amazonviewer/vista/AplicationMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsuntaxi.amazonviewer.vista;
+
+/**
+ *
+ * @author j_h4e
+ */
+public class AplicationMain {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
