ea77a104b55c399224789f21d8a40fd2d024dd94
j_h4e <j_h4e@DESKTOP-A42TOC9.mshome.net>
2018-10-26 11:58:42 -0500

    Se declararon las clases TDA bases para Amazon Viewer

---
+++ src/com/jsuntaxi/amazonviewer/modelo/Serie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsuntaxi.amazonviewer.modelo;
+
+/**
+ *
+ * @author j_h4e
+ */
+public class Serie {
+    
+}
